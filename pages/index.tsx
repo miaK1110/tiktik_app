4c7eacc54bcfcd564a462aa05f40affd09b5c97b
mia <m.09048030369@gmail.com>
2022-12-27 23:30:38 +0900

    added videocard and noresults component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,25 @@
 import type { NextPage } from 'next';
 import axios from 'axios';
+
 import { Video } from '../types';
+import VideoCard from '../components/VideoCard';
+import NoResults from '../components/NoResults';
 
 interface IProps {
   videos: Video[];
 }
 
 const Home = ({ videos }: IProps) => {
-  return <h1 className='text-3xl font-bold underline'>Test</h1>;
+  console.log(videos);
+  return (
+    <div className='flex flex-col gap-10 videos h-full'>
+      {videos.length ? (
+        videos.map((video: Video) => <VideoCard post={video} key={video._id} />)
+      ) : (
+        <NoResults text={'No Videos'} />
+      )}
+    </div>
+  );
 };
 
 export const getServerSideProps = async () => {
