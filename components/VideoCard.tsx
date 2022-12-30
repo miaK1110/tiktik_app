700d800279eb8db4a356796931945230fe24b3e9
mia <m.09048030369@gmail.com>
2022-12-31 00:06:05 +0900

    changed main page

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -12,6 +12,10 @@ interface IProps {
 }
 
 const VideoCard: NextPage<IProps> = ({ post }) => {
+  const [isHover, setisHover] = useState(false);
+  const [playing, setplaying] = useState(false);
+  const [isVideoMuted, setisVideoMuted] = useState(false);
+
   return (
     <div className='flex flex-col border-b-2 border-gray-200 pb-6'>
       <div className=''>
@@ -31,6 +35,64 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
               </>
             </Link>
           </div>
+          <div className='flex items-center gap-2'>
+            <Link href='/'>
+              <div className='flex gap-2 items-center md:text-md font-bold text-primary'>
+                <p>
+                  {post.postedBy.userName}
+                  {''}
+                  <GoVerified
+                    className='text-blue-400 text-md
+   '
+                  />
+                </p>
+                <p className='capitalize font-medium text-xs text-gray-500 hidden md:block'>
+                  {post.postedBy.userName}
+                </p>
+              </div>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className='lg:ml-20 flex gap-4 relative'>
+        <div
+          onMouseEnter={() => {
+            setisHover(true);
+          }}
+          onMouseLeave={() => {
+            setisHover(false);
+          }}
+          className='rounded-3xl'
+        >
+          <Link href='/'>
+            <video
+              className='lg:w[600px] h-[300px] md:h-[400px] lg:h-[530px] w-[200px] rounded-2xl cursor-pointer bg-gray-100'
+              loop
+              src={post.video.asset.url}
+            ></video>
+          </Link>
+          {isHover && (
+            <div>
+              {playing ? (
+                <button>
+                  <BsFillPauseFill className='text-black text-2xl lg:text-4xl' />
+                </button>
+              ) : (
+                <button>
+                  <BsFillPlayFill className='text-black text-2xl lg:text-4xl' />
+                </button>
+              )}
+              {isVideoMuted ? (
+                <button>
+                  <HiVolumeOff className='text-black text-2xl lg:text-4xl' />
+                </button>
+              ) : (
+                <button>
+                  <HiVolumeUp className='text-black text-2xl lg:text-4xl' />
+                </button>
+              )}
+            </div>
+          )}
         </div>
       </div>
     </div>
--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   optimizeFonts: true,
+  // images: {
+  //   domains: [],
+  // },
 };
 
 module.exports = nextConfig;
