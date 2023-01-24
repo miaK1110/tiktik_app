64f933958346a1d6e86af2bf5ac274540d034178
mia <m.09048030369@gmail.com>
2023-01-24 23:46:46 +0900

    changed video detail page

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -1,113 +1,145 @@
 // in next.js [] means that the url is going to be dynamic
-
 import React, { useEffect, useRef, useState } from 'react';
 import { useRouter } from 'next/router';
+import { GoVerified } from 'react-icons/go';
 import Image from 'next/image';
 import Link from 'next/link';
-import { GoVerified } from 'react-icons/go';
 import { MdOutlineCancel } from 'react-icons/md';
 import { BsFillPlayFill } from 'react-icons/bs';
 import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi';
-import axios from 'axios';
 
 import { BASE_URL } from '../../utils';
+import useAuthStore from '../../store/authStore';
 import { Video } from '../../types';
+import axios from 'axios';
 
 interface IProps {
   postDetails: Video;
 }
 
 const Detail = ({ postDetails }: IProps) => {
-  const [post, setpost] = useState(postDetails);
-  const [playing, setplaying] = useState(false);
+  const [post, setPost] = useState(postDetails);
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+  const [isVideoMuted, setIsVideoMuted] = useState<boolean>(false);
+  const [isPostingComment, setIsPostingComment] = useState<boolean>(false);
+  const [comment, setComment] = useState<string>('');
+
   const videoRef = useRef<HTMLVideoElement>(null);
-  const [isVideoMuted, setisVideoMuted] = useState(false);
   const router = useRouter();
 
+  const { userProfile }: any = useAuthStore();
+
+  const onVideoClick = () => {
+    if (isPlaying) {
+      videoRef?.current?.pause();
+      setIsPlaying(false);
+    } else {
+      videoRef?.current?.play();
+      setIsPlaying(true);
+    }
+  };
+
   useEffect(() => {
-    // if we have valid video selected
     if (post && videoRef?.current) {
       videoRef.current.muted = isVideoMuted;
     }
   }, [post, isVideoMuted]);
 
-  const onVideoClick = () => {
-    if (playing) {
-      videoRef.current?.pause();
-      setplaying(false);
-    } else {
-      videoRef.current?.play();
-      setplaying(true);
+  const handleLike = async (like: boolean) => {
+    if (userProfile) {
+      const res = await axios.put(`${BASE_URL}/api/like`, {
+        userId: userProfile._id,
+        postId: post._id,
+        like,
+      });
+      setPost({ ...post, likes: res.data.likes });
     }
   };
 
-  if (!post) return null;
-  return (
-    <div className='flex w-full absolute left-0 top-0 bg-white flex-wrap lg:flex-nowrap'>
-      <div className='relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-black'>
-        <div className='absolute top-6 left-6 flex gap-6 z-50'>
-          <p className='cursor-pointer'>
-            <MdOutlineCancel className='text-white text-[35px]' />
-          </p>
-        </div>
-        <div
-          className='relative'
-          onClick={() => {
-            router.back();
-          }}
-        >
-          <div className='lg:h-[100vh] h-[60vh]'>
-            <video
-              ref={videoRef}
-              src={post.video.asset.url}
-              onClick={onVideoClick}
-              className='h-full cursor-pointer'
-            ></video>
-          </div>
-          <div className='absolute top-[50%] left-[45%] cursor-pointer'>
-            {!playing && (
-              <button>
-                <BsFillPlayFill
-                  className='text-white text-6xl lg:text-8xl'
-                  onClick={onVideoClick}
-                />
-              </button>
-            )}
-          </div>
-        </div>
-        <div className='absolute bottom-5 lg:bottom-10 right-5 lg:right-10 cursor-pointer'>
-          {isVideoMuted ? (
-            <button onClick={() => setisVideoMuted(true)}>
-              <HiVolumeOff className='text-black text-2xl lg:text-4xl' />
-            </button>
-          ) : (
-            <button onClick={() => setisVideoMuted(false)}>
-              <HiVolumeUp className='text-black text-2xl lg:text-4xl' />
-            </button>
-          )}
-        </div>
-      </div>
+  const addComment = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
 
-      <div className='relative w-[1000px] md:w-[900px] lg-[700px]'>
-        <div className='lg:mt-20 mt-10 '>
-          <div className='md:w-16 md:h-16 w-10 h-10'>
-            <Link href='/'>
-              {/* can't put image as a child component of a Link */}
-              <>
-                <Image
-                  width={62}
-                  height={62}
-                  className='rounded-full'
-                  src={post.postedBy.image}
-                  alt='profile shoot'
-                  layout='responsive'
-                />
-              </>
-            </Link>
+    if (userProfile) {
+      if (comment) {
+        setIsPostingComment(true);
+        const res = await axios.put(`${BASE_URL}/api/post/${post._id}`, {
+          userId: userProfile._id,
+          comment,
+        });
+
+        setPost({ ...post, comments: res.data.comments });
+        setComment('');
+        setIsPostingComment(false);
+      }
+    }
+  };
+
+  return (
+    <>
+      {post && (
+        <div className='flex w-full absolute left-0 top-0 bg-white flex-wrap lg:flex-nowrap'>
+          <div className='relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-blurred-img bg-no-repeat bg-cover bg-center'>
+            <div className='opacity-90 absolute top-6 left-2 lg:left-6 flex gap-6 z-50'>
+              <p className='cursor-pointer ' onClick={() => router.back()}>
+                <MdOutlineCancel className='text-white text-[35px] hover:opacity-90' />
+              </p>
+            </div>
+            <div className='relative'>
+              <div className='lg:h-[100vh] h-[60vh]'>
+                <video
+                  ref={videoRef}
+                  onClick={onVideoClick}
+                  loop
+                  src={post?.video?.asset.url}
+                  className=' h-full cursor-pointer'
+                ></video>
+              </div>
+
+              <div className='absolute top-[45%] left-[40%]  cursor-pointer'>
+                {!isPlaying && (
+                  <button onClick={onVideoClick}>
+                    <BsFillPlayFill className='text-white text-6xl lg:text-8xl' />
+                  </button>
+                )}
+              </div>
+            </div>
+            <div className='absolute bottom-5 lg:bottom-10 right-5 lg:right-10  cursor-pointer'>
+              {isVideoMuted ? (
+                <button onClick={() => setIsVideoMuted(false)}>
+                  <HiVolumeOff className='text-white text-3xl lg:text-4xl' />
+                </button>
+              ) : (
+                <button onClick={() => setIsVideoMuted(true)}>
+                  <HiVolumeUp className='text-white text-3xl lg:text-4xl' />
+                </button>
+              )}
+            </div>
+          </div>
+          <div className='relative w-[1000px] md:w-[900px] lg:w-[700px]'>
+            <div className='lg:mt-20 mt-10'>
+              <Link href={`/profile/${post.postedBy._id}`}>
+                <div className='flex gap-4 mb-4 bg-white w-full pl-10 cursor-pointer'>
+                  <Image
+                    width={60}
+                    height={60}
+                    alt='user-profile'
+                    className='rounded-full'
+                    src={post.postedBy.image}
+                  />
+                  <div>
+                    <div className='text-xl font-bold lowercase tracking-wider flex gap-2 items-center justify-center'>
+                      {post.postedBy.userName.replace(/\s+/g, '')}{' '}
+                      <GoVerified className='text-blue-400 text-xl' />
+                    </div>
+                    <p className='text-md'> {post.postedBy.userName}</p>
+                  </div>
+                </div>
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
@@ -116,9 +148,11 @@ export const getServerSideProps = async ({
 }: {
   params: { id: string };
 }) => {
-  const { data } = await axios.get(`${BASE_URL}/api/post/${id}`);
+  const res = await axios.get(`${BASE_URL}/api/post/${id}`);
+
   return {
-    props: { postDetails: data },
+    props: { postDetails: res.data },
   };
 };
+
 export default Detail;
